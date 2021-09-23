eb5917dea62c363ea7644460c35ebd2726c13e72
ThinkingFrog <khripunkovdv@gmail.com>
2021-09-23 23:58:10 +0300

    Draw average signal data

--- Coursework/lab/main.py
+++ Coursework/lab/main.py
@@ -1,6 +1,8 @@
 from typing import List
 from pathlib import Path
 
+from numpy import sign
+
 from lab.manager import SignalManager
 
 
@@ -16,25 +18,32 @@ def main():
     level3_path = data_dir / "+0_5V"
     level4_path = data_dir / "+0_25V"
 
-    signal_data: List[List[float]] = list()
-    level0_data: List[List[float]] = list()
-    level1_data: List[List[float]] = list()
-    level2_data: List[List[float]] = list()
-    level3_data: List[List[float]] = list()
-    level4_data: List[List[float]] = list()
+    signal_data: List[List[float]] = manager.read_signal(signal_path)
+    level0_data: List[List[List[float]]] = list()
+    level1_data: List[List[List[float]]] = list()
+    level2_data: List[List[List[float]]] = list()
+    level3_data: List[List[List[float]]] = list()
+    level4_data: List[List[List[float]]] = list()
 
     for data_path, data_array in zip(
         [
-            signal_path,
             level0_path,
             level1_path,
             level2_path,
             level3_path,
             level4_path,
         ],
-        [signal_data, level0_data, level1_data, level2_data, level3_data, level4_data],
+        [level0_data, level1_data, level2_data, level3_data, level4_data],
     ):
         for data_file in data_path.iterdir():
-            data_array.append(manager.read_signal(data_file))
+            data_array.append(manager.read_file(data_file))
 
-    print(len(signal_data[0]))
+    manager.draw_average(
+        [level0_data, level1_data, level2_data, level3_data, level4_data]
+    )
+
+    manager.draw_average_with_signal(
+        signal_data, [level0_data, level1_data, level2_data, level3_data, level4_data]
+    )
+
+    manager.draw_signal_interpolation(None)
--- Coursework/lab/manager.py
+++ Coursework/lab/manager.py
@@ -8,12 +8,19 @@ import numpy as np
 
 class SignalManager:
     _signal_length: int
+    _signal_part: int
 
     def __init__(self) -> None:
         self._signal_length = 1024
+        self._signal_part = 17
 
-    def read_signal(self, filepath: Path) -> List[float]:
-        signal: List[float] = list()
+    def read_signal(self, dirpath: Path) -> List[float]:
+        for idx, data_file in enumerate(dirpath.iterdir()):
+            if idx == self._signal_part:
+                return self.read_file(data_file)
+
+    def read_file(self, filepath: Path) -> List[List[float]]:
+        signal: List[List[float]] = list()
         with filepath.open("r") as file:
             for line_num, line in enumerate(file):
                 if line_num == 0:
@@ -22,5 +29,41 @@ class SignalManager:
                 if line_num > self._signal_length:
                     break
 
-                signal += list(map(lambda x: float(x.strip()), line.split()[1:]))
+                signal.append(list(map(lambda x: float(x.strip()), line.split()[1:])))
         return signal
+
+    def draw_average(self, data_array: List[List[List[List[float]]]]) -> None:
+        for data in data_array:
+            x, y = list(), list()
+            average_by_line = [[] for _ in range(self._signal_length)]
+
+            for file_data in data:
+                for line_idx, line_data in enumerate(file_data):
+                    average_by_line[line_idx].append(np.mean(line_data))
+
+            for idx, ave in enumerate(average_by_line):
+                x.append(np.mean(ave))
+                y.append(idx)
+            plt.xlabel("Measuring number")
+            plt.ylabel("Average signal value")
+            plt.plot(y, x)
+
+        plt.show()
+
+    def draw_average_with_signal(
+        self, signal_data: List[List[float]], data_array: List[List[List[List[float]]]]
+    ) -> None:
+        x, y = list(), list()
+
+        for idx, subarr in enumerate(signal_data):
+            x.append(np.mean(subarr))
+            y.append(idx)
+
+        plt.xlabel("Measuring number")
+        plt.ylabel("Average signal value")
+        plt.plot(y, x)
+
+        self.draw_average(data_array)
+
+    def draw_signal_interpolation(self, signal_data: List[List[float]]) -> None:
+        pass
