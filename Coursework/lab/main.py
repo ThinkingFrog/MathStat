797aa6932ed245446462556d08af104f5c238633
ThinkingFrog <khripunkovdv@gmail.com>
2021-09-26 15:53:42 +0300

    Add interpolation

--- Coursework/lab/main.py
+++ Coursework/lab/main.py
@@ -16,8 +16,8 @@ def main():
     level0_path = data_dir / "ZeroLine"
     level1_path = data_dir / "-0_5V"
     level2_path = data_dir / "-0_25V"
-    level3_path = data_dir / "+0_5V"
-    level4_path = data_dir / "+0_25V"
+    level3_path = data_dir / "+0_25V"
+    level4_path = data_dir / "+0_5V"
 
     signal_data = manager.read_signal(signal_path, signal_part)
     level0_data = manager.read_dir(level0_path)
@@ -32,3 +32,14 @@ def main():
     level2_data = manager.get_average(level2_data)
     level3_data = manager.get_average(level3_data)
     level4_data = manager.get_average(level4_data)
+
+    manager.plot([level0_data, level1_data, level2_data, level3_data, level4_data])
+
+    manager.plot(
+        [signal_data, level0_data, level1_data, level2_data, level3_data, level4_data]
+    )
+
+    levels = [-0.5, -0.25, 0, 0.25, 0.5]
+    consts = np.array([level1_data, level2_data, level0_data, level3_data, level4_data])
+    signal_data = manager.interpolation(signal_data, levels, consts)
+    manager.plot([signal_data])
--- Coursework/lab/manager.py
+++ Coursework/lab/manager.py
@@ -31,20 +31,12 @@ class SignalManager:
             if val > averages[idx] and val < averages[idx + 1]:
                 return idx
 
-    def draw_signal(
-        self,
-        signal_data: List[float],
-    ) -> None:
-
-        plt.plot(range(len(signal_data)), signal_data)
-        plt.show()
-
-    def read_signal(self, dirpath: Path, signal_part: int) -> np.array:
+    def read_signal(self, dirpath: Path, signal_part: int) -> np.ndarray:
         for idx, data_file in enumerate(dirpath.iterdir()):
             if idx == signal_part:
                 return self.read_file(data_file)
 
-    def read_file(self, filepath: Path) -> np.array:
+    def read_file(self, filepath: Path) -> np.ndarray:
         with filepath.open("r") as file:
             data = np.zeros((self._signal_length, self._subarray_size))
             for line_num, line in enumerate(file):
@@ -59,14 +51,33 @@ class SignalManager:
                 ] = list(map(lambda x: float(x.strip()), line.split()[1:]))
         return data
 
-    def read_dir(self, dirpath: Path) -> np.array:
+    def read_dir(self, dirpath: Path) -> np.ndarray:
         data = np.zeros(
-            (len(dirpath.glob("*.txt")), self._signal_length, self._subarray_size)
+            (len(list(dirpath.glob("*.txt"))), self._signal_length, self._subarray_size)
         )
         for idx, data_file in enumerate(dirpath.glob("*.txt")):
             data[idx] = self.read_file(data_file)
         return data
 
-    def get_average(self, data_array: np.array) -> np.ndarray:
+    def get_average(self, data_array: np.ndarray) -> np.ndarray:
         data_array = np.mean(data_array, axis=2)
         return np.mean(data_array, axis=0)
+
+    def interpolation(self, data, dc, constants):
+        result = np.zeros(len(data))
+
+        for idx in range(len(data)):
+            id = self._get_level_idx(data[idx], constants[:, idx])
+            result[idx] = self._interpolate(
+                [constants[id, idx], constants[id + 1, idx]],
+                [dc[id], dc[id + 1]],
+                data[idx],
+            )
+
+        return result
+
+    def plot(self, data_arrays: List[np.ndarray]) -> None:
+        for data in data_arrays:
+            plt.plot(range(data.size), data)
+
+        plt.show()
