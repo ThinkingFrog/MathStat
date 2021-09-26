c5faa03ebe627915dc88bf179f292e6d01e10e36
ThinkingFrog <khripunkovdv@gmail.com>
2021-09-26 14:52:34 +0300

    Rewrite all methods to use numpy arrays

--- Coursework/lab/main.py
+++ Coursework/lab/main.py
@@ -1,7 +1,7 @@
 from typing import List
 from pathlib import Path
 
-from numpy import sign
+import numpy as np
 
 from lab.manager import SignalManager
 
@@ -12,46 +12,23 @@ def main():
     data_dir = Path("data")
 
     signal_path = data_dir / "Sin_100MHz"
+    signal_part = 17
     level0_path = data_dir / "ZeroLine"
     level1_path = data_dir / "-0_5V"
     level2_path = data_dir / "-0_25V"
     level3_path = data_dir / "+0_5V"
     level4_path = data_dir / "+0_25V"
 
-    signal_data: List[List[float]] = manager.read_signal(signal_path)
-    level0_data: List[List[List[float]]] = list()
-    level1_data: List[List[List[float]]] = list()
-    level2_data: List[List[List[float]]] = list()
-    level3_data: List[List[List[float]]] = list()
-    level4_data: List[List[List[float]]] = list()
+    signal_data = manager.read_signal(signal_path, signal_part)
+    level0_data = manager.read_dir(level0_path)
+    level1_data = manager.read_dir(level1_path)
+    level2_data = manager.read_dir(level2_path)
+    level3_data = manager.read_dir(level3_path)
+    level4_data = manager.read_dir(level4_path)
 
-    for data_path, data_array in zip(
-        [
-            level0_path,
-            level1_path,
-            level2_path,
-            level3_path,
-            level4_path,
-        ],
-        [level0_data, level1_data, level2_data, level3_data, level4_data],
-    ):
-        for data_file in data_path.iterdir():
-            data_array.append(manager.read_file(data_file))
-
-    manager.draw_average(
-        [level0_data, level1_data, level2_data, level3_data, level4_data]
-    )
-
-    manager.draw_average_with_signal(
-        signal_data, [level0_data, level1_data, level2_data, level3_data, level4_data]
-    )
-
-    manager.draw_signal_interpolation(
-        signal_data,
-        manager.get_average(
-            [level0_data, level1_data, level2_data, level3_data, level4_data]
-        ),
-        [-0.5, -0.25, 0.0, 0.25, 0.5],
-    )
-
-    manager.draw_scale(signal_data)
+    signal_data = np.mean(signal_data, axis=1)
+    level0_data = manager.get_average(level0_data)
+    level1_data = manager.get_average(level1_data)
+    level2_data = manager.get_average(level2_data)
+    level3_data = manager.get_average(level3_data)
+    level4_data = manager.get_average(level4_data)
--- Coursework/lab/manager.py
+++ Coursework/lab/manager.py
@@ -4,83 +4,21 @@ from typing import List, Tuple
 import matplotlib.pyplot as plt
 import numpy as np
 
+from lab.lib.tolsolvty import tolsolvty
+
 
 class SignalManager:
     _signal_length: int
-    _signal_part: int
+    _subarray_size: int
 
     def __init__(self) -> None:
         self._signal_length = 1024
-        self._signal_part = 17
+        self._subarray_size = 9
 
     def _interpolate(
         self, x: Tuple[float, float], y: Tuple[float, float], x_val: float
     ) -> float:
         return y[0] + (x_val - x[0]) / (x[1] - x[0]) * (y[1] - y[0])
-
-    def read_signal(self, dirpath: Path) -> List[float]:
-        for idx, data_file in enumerate(dirpath.iterdir()):
-            if idx == self._signal_part:
-                return self.read_file(data_file)
-
-    def read_file(self, filepath: Path) -> List[List[float]]:
-        signal: List[List[float]] = list()
-        with filepath.open("r") as file:
-            for line_num, line in enumerate(file):
-                if line_num == 0:
-                    stop_pos = int(line.split()[-1].strip())
-                    continue
-                if line_num > self._signal_length:
-                    break
-
-                signal.append(list(map(lambda x: float(x.strip()), line.split()[1:])))
-        return signal
-
-    def get_average(self, data_array: List[List[List[List[float]]]]) -> np.ndarray:
-        ave_arrays = list()
-
-        for data in data_array:
-            averages = list()
-            average_by_line = [[] for _ in range(self._signal_length)]
-
-            for file_data in data:
-                for line_idx, line_data in enumerate(file_data):
-                    average_by_line[line_idx].append(np.mean(line_data))
-
-            for ave in average_by_line:
-                averages.append(np.mean(ave))
-            ave_arrays.append(np.asarray(averages))
-
-        return np.asarray(ave_arrays)
-
-    def draw_average(self, data_array: List[List[List[List[float]]]]) -> None:
-        for data in data_array:
-            x, y = list(), list()
-            average_by_line = [[] for _ in range(self._signal_length)]
-
-            for file_data in data:
-                for line_idx, line_data in enumerate(file_data):
-                    average_by_line[line_idx].append(np.mean(line_data))
-
-            for idx, ave in enumerate(average_by_line):
-                x.append(np.mean(ave))
-                y.append(idx)
-            plt.xlabel("Measuring number")
-            plt.ylabel("Average signal value")
-            plt.plot(y, x)
-
-        plt.show()
-
-    def draw_average_with_signal(
-        self, signal_data: List[List[float]], data_array: List[List[List[List[float]]]]
-    ) -> None:
-        signal_average_data = [np.mean(subarr) for subarr in signal_data]
-
-        plt.xlabel("Measuring number")
-        plt.ylabel("Average signal value")
-        plt.plot(range(len(signal_average_data)), signal_average_data)
-
-        self.draw_average(data_array)
 
     def _get_level_idx(self, val: float, averages: np.ndarray) -> int:
         if val < averages[0]:
@@ -93,44 +31,42 @@ class SignalManager:
             if val > averages[idx] and val < averages[idx + 1]:
                 return idx
 
-    def draw_signal_interpolation(
+    def draw_signal(
         self,
-        signal_data: List[List[float]],
-        averages: np.ndarray,
-        levels: List[float],
+        signal_data: List[float],
     ) -> None:
-        signal_interpolation = list()
-        signal_average_data = [np.mean(subarr) for subarr in signal_data]
 
-        for idx in range(len(signal_average_data)):
-            level_idx = self._get_level_idx(signal_average_data[idx], averages[:, idx])
-            signal_interpolation.append(
-                self._interpolate(
-                    [averages[level_idx, idx], averages[level_idx + 1, idx]],
-                    [levels[level_idx], levels[level_idx + 1]],
-                    signal_average_data[idx],
-                )
-            )
-
-        plt.plot(range(len(signal_interpolation)), signal_interpolation)
+        plt.plot(range(len(signal_data)), signal_data)
         plt.show()
 
-    def get_scaled(self, signal_data: List[List[float]]) -> List[float]:
-        signal_average_data = [np.mean(subarr) for subarr in signal_data]
-        up_bound = max(signal_average_data)
-        lo_bound = min(signal_average_data)
+    def read_signal(self, dirpath: Path, signal_part: int) -> np.array:
+        for idx, data_file in enumerate(dirpath.iterdir()):
+            if idx == signal_part:
+                return self.read_file(data_file)
 
-        signal_scale_data = list()
-        for val in signal_average_data:
-            if val > 0:
-                signal_scale_data.append(val / up_bound)
-            elif val < 0:
-                signal_scale_data.append(-val / lo_bound)
+    def read_file(self, filepath: Path) -> np.array:
+        with filepath.open("r") as file:
+            data = np.zeros((self._signal_length, self._subarray_size))
+            for line_num, line in enumerate(file):
+                if line_num == 0:
+                    stop_pos = int(line.split()[-1].strip())
+                    continue
+                if line_num > self._signal_length:
+                    break
 
-        return signal_scale_data
+                data[
+                    (self._signal_length + line_num - stop_pos) % self._signal_length
+                ] = list(map(lambda x: float(x.strip()), line.split()[1:]))
+        return data
 
-    def draw_scale(self, signal_data: List[List[float]]) -> None:
-        signal_scale_data = self.get_scaled(signal_data)
+    def read_dir(self, dirpath: Path) -> np.array:
+        data = np.zeros(
+            (len(dirpath.glob("*.txt")), self._signal_length, self._subarray_size)
+        )
+        for idx, data_file in enumerate(dirpath.glob("*.txt")):
+            data[idx] = self.read_file(data_file)
+        return data
 
-        plt.plot(range(len(signal_scale_data)), signal_scale_data)
-        plt.show()
+    def get_average(self, data_array: np.array) -> np.ndarray:
+        data_array = np.mean(data_array, axis=2)
+        return np.mean(data_array, axis=0)
