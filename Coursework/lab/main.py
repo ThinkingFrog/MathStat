a620f575436e022c4ee0564475f865a7957f12e6
ThinkingFrog <khripunkovdv@gmail.com>
2021-09-26 23:38:31 +0300

    Add final program

--- Coursework/lab/main.py
+++ Coursework/lab/main.py
@@ -12,7 +12,7 @@ def main():
     data_dir = Path("data")
 
     signal_path = data_dir / "Sin_100MHz"
-    signal_part = 17
+    signal_part = 7
     level0_path = data_dir / "ZeroLine"
     level1_path = data_dir / "-0_5V"
     level2_path = data_dir / "-0_25V"
@@ -20,6 +20,7 @@ def main():
     level4_path = data_dir / "+0_5V"
 
     signal_data = manager.read_signal(signal_path, signal_part)
+    all_signals_data = manager.read_dir(signal_path)
     level0_data = manager.read_dir(level0_path)
     level1_data = manager.read_dir(level1_path)
     level2_data = manager.read_dir(level2_path)
@@ -27,19 +28,73 @@ def main():
     level4_data = manager.read_dir(level4_path)
 
     signal_data = np.mean(signal_data, axis=1)
+    all_signals_data = manager.get_average(all_signals_data)
     level0_data = manager.get_average(level0_data)
     level1_data = manager.get_average(level1_data)
     level2_data = manager.get_average(level2_data)
     level3_data = manager.get_average(level3_data)
     level4_data = manager.get_average(level4_data)
 
-    manager.plot([level0_data, level1_data, level2_data, level3_data, level4_data])
+    manager.plot(
+        [level0_data, level1_data, level2_data, level3_data, level4_data],
+        title="Amplitudes",
+        xlabel="Measurement",
+        ylabel="Amplitude value",
+    )
 
     manager.plot(
-        [signal_data, level0_data, level1_data, level2_data, level3_data, level4_data]
+        [signal_data, level0_data, level1_data, level2_data, level3_data, level4_data],
+        title="Amplitudes with signal",
+        xlabel="Measurement",
+        ylabel="Amplitude value",
     )
 
     levels = [-0.5, -0.25, 0, 0.25, 0.5]
-    consts = np.array([level1_data, level2_data, level0_data, level3_data, level4_data])
-    signal_data = manager.interpolation(signal_data, levels, consts)
-    manager.plot([signal_data])
+    levels_data = np.array(
+        [level1_data, level2_data, level0_data, level3_data, level4_data]
+    )
+    signal_data = manager.get_interpolated(signal_data, levels, levels_data)
+    all_signals_data = manager.get_interpolated(all_signals_data, levels, levels_data)
+    manager.plot(
+        [signal_data],
+        title="Interpolated signal",
+        xlabel="Measurement",
+        ylabel="Amplitude value",
+    )
+
+    sin_data = manager.get_sin(signal_data)
+    all_sin_data = manager.get_sin(all_signals_data)
+    manager.plot(
+        [sin_data],
+        title="Interpolated signal on [0, 1] segment",
+        xlabel="Measurement",
+        ylabel="Amplitude value",
+    )
+
+    arcsin_data = np.arcsin(sin_data)
+    manager.plot(
+        [arcsin_data],
+        title="Signal arcsinus",
+        xlabel="Measurement",
+        ylabel="Amplitude value",
+    )
+
+    elements = manager.get_partition(arcsin_data)
+    [ampl, a1, b1, a2, b2] = manager.get_asin_amp(arcsin_data, elements)
+    manager.plot_signal_with_lines(
+        arcsin_data,
+        a1,
+        b1,
+        a2,
+        b2,
+        50,
+        title="Signal amplitude",
+        xlabel="Measurement",
+        ylabel="Amplitude value",
+    )
+
+    signal_data = manager.set_amplitude(sin_data, ampl)
+    all_signals_data = manager.set_amplitude(all_sin_data, ampl)
+
+    manager.plot_time_delta(signal_data, 40, title="Time periods", xlabel="Time")
+    manager.hist(all_signals_data, title="Time histogram", xlabel="Time")
--- Coursework/lab/manager.py
+++ Coursework/lab/manager.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 from typing import List, Tuple
 
@@ -63,21 +64,173 @@ class SignalManager:
         data_array = np.mean(data_array, axis=2)
         return np.mean(data_array, axis=0)
 
-    def interpolation(self, data, dc, constants):
-        result = np.zeros(len(data))
+    def get_interpolated(self, data, levels, levels_data) -> np.ndarray:
+        interpolated = np.zeros(len(data))
 
         for idx in range(len(data)):
-            id = self._get_level_idx(data[idx], constants[:, idx])
-            result[idx] = self._interpolate(
-                [constants[id, idx], constants[id + 1, idx]],
-                [dc[id], dc[id + 1]],
+            level_idx = self._get_level_idx(data[idx], levels_data[:, idx])
+            interpolated[idx] = self._interpolate(
+                [levels_data[level_idx, idx], levels_data[level_idx + 1, idx]],
+                [levels[level_idx], levels[level_idx + 1]],
                 data[idx],
             )
 
-        return result
+        return interpolated
 
-    def plot(self, data_arrays: List[np.ndarray]) -> None:
+    def get_sin(self, data):
+        sin_data = np.divide(data, max(abs(max(data)), abs(min(data))))
+        sin_data = np.add(sin_data, 1)
+        sin_data = np.divide(sin_data, max(sin_data))
+        return sin_data
+
+    def get_partition(self, data):
+        elements = [[], []]
+
+        if data[0] < data[1]:
+            elements[0].append(0)
+        else:
+            elements[1].append(0)
+
+        for idx in range(1, len(data)):
+            if data[idx] >= data[idx - 1]:
+                elements[0].append(idx)
+            else:
+                elements[1].append(idx)
+
+        return elements
+
+    def get_asin_amp(self, bin_val, ids):
+        dy = 0.015
+        di = 1 / 2
+        A2_bot = np.zeros((len(ids[1]), 3))
+        A2_top = np.zeros((len(ids[1]), 3))
+        B2_bot = np.zeros((len(ids[1]), 1))
+        B2_top = np.zeros((len(ids[1]), 1))
+
+        A1_bot = np.zeros((len(ids[0]), 3))
+        A1_top = np.zeros((len(ids[0]), 3))
+        B1_bot = np.zeros((len(ids[0]), 1))
+        B1_top = np.zeros((len(ids[0]), 1))
+
+        count = 0
+
+        for i in range(len(ids[0])):
+            if i != 0 and ids[0][i] - ids[0][i - 1] > 2:
+                count += 1
+
+            A1_bot[i, 0] = ids[0][i] - di + 1
+            A1_bot[i, 1] = 1
+            A1_bot[i, 2] = count
+            B1_bot[i, 0] = bin_val[ids[0][i]] - dy * abs(bin_val[ids[0][i]])
+
+            A1_top[i, 0] = ids[0][i] + di + 1
+            A1_top[i, 1] = 1
+            A1_top[i, 2] = count
+            B1_top[i, 0] = bin_val[ids[0][i]] + dy * abs(bin_val[ids[0][i]])
+
+        count = 0
+
+        for i in range(len(ids[1])):
+            if i != 0 and ids[1][i] - ids[1][i - 1] > 2:
+                count += 1
+
+            A2_bot[i, 0] = ids[1][i] - di
+            A2_bot[i, 1] = 1
+            A2_bot[i, 2] = count
+            B2_bot[i, 0] = bin_val[ids[1][i]] - dy * abs(bin_val[ids[1][i]])
+
+            A2_top[i, 0] = ids[1][i] + di
+            A2_top[i, 1] = 1
+            A2_top[i, 2] = count
+            B2_top[i, 0] = bin_val[ids[1][i]] + dy * abs(bin_val[ids[1][i]])
+
+        [tolmax, argmax, envs, ccode] = tolsolvty(A1_bot, A1_top, B1_bot, B1_top)
+        a1 = argmax[0]
+        b1 = argmax[1]
+        [tolmax, argmax, envs, ccode] = tolsolvty(A2_bot, A2_top, B2_bot, B2_top)
+        a2 = argmax[0]
+        b2 = argmax[1]
+        y = abs((b1 * a2 - b2 * a1) / (a2 - a1))
+
+        return [y, a1, b1, a2, b2]
+
+    def set_amplitude(self, data: np.ndarray, coefficient: float) -> np.ndarray:
+        new_data = np.divide(data, max(abs(max(data)), abs(min(data))))
+        return np.multiply(new_data, coefficient)
+
+    def plot(
+        self,
+        data_arrays: List[np.ndarray],
+        title: str = "",
+        xlabel: str = "",
+        ylabel: str = "",
+    ) -> None:
         for data in data_arrays:
             plt.plot(range(data.size), data)
 
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        plt.show()
+
+    def plot_signal_with_lines(
+        self,
+        signal_data: np.ndarray,
+        a1: float,
+        b1: float,
+        a2: float,
+        b2: float,
+        num_of_dots: int,
+        title: str = "",
+        xlabel: str = "",
+        ylabel: str = "",
+    ) -> None:
+        x = np.zeros(num_of_dots)
+        y1 = np.zeros(num_of_dots)
+        y2 = np.zeros(num_of_dots)
+        for i in range(num_of_dots):
+            x[i] = i
+            y1[i] = a1 * x[i] + b1
+            y2[i] = a2 * x[i] + b2
+
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.plot(x, y1, "g")
+        plt.plot(x, y2, "r")
+        self.plot([signal_data], title)
+
+    def plot_time_delta(
+        self,
+        data: np.ndarray,
+        num_of_dots: int,
+        title: str = "",
+        xlabel: str = "",
+        ylabel: str = "",
+    ) -> None:
+        t_i = np.zeros(num_of_dots)
+        for idx in range(num_of_dots):
+            t_i[idx] = (data[idx + 1] - data[idx]) / (2 * math.pi)
+        t_i = np.add(t_i, abs(min(t_i)))
+
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        plt.xlabel("time(ps)")
+        plt.scatter(t_i, [1 for _ in range(num_of_dots)], marker=".")
+        plt.show()
+
+    def hist(
+        self,
+        data: np.ndarray,
+        title: str = "",
+        xlabel: str = "",
+        ylabel: str = "",
+    ) -> None:
+        y = [el / (2 * math.pi) for el in data]
+
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        plt.xlabel("time")
+        plt.hist(y, bins=16)
         plt.show()
